3f87066037b52d7e96795fa370a4b3a205005bc4
Matvey Dolgodrov <matvey@Matveys-MacBook-Pro-2.local>
2022-09-16 23:59:33 +0300

    logic

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const cors = require("cors");
+// const engineLogic = require("./engineLogic");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
+app.listen(3000);
 
-// test2
+app.post("/forward", (req, res) => {
+  const message = req.body;
+  console.log(req.body);
+//   engineLogic.forward();
+  res.send("Forward");
+});
+
+app.post("/backward", (req, res) => {
+  const message = req.body;
+  console.log(req.body);
+  res.send("Backward");
+});
+
+app.post("/right", (req, res) => {
+  const message = req.body;
+  console.log(req.body);
+  res.send("Right");
+});
+
+app.post("/left", (req, res) => {
+  const message = req.body;
+  console.log(req.body);
+  res.send("Left");
+});
